b3a15b294263521c458604759a1e81514da8e072
CastorDisaster <alexmnevins@gmail.com>
2024-04-10 10:07:09 -0400

    refactor to class-based approach

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('here!')
+
 
 class Node {
     constructor (d) {
@@ -7,46 +7,54 @@ class Node {
         this.right = null;
     }
 }
-let root = null
-function sortedArrayToBST (arr, start = 0, end = arr.length - 1) { 
-    //takes an array, and a start and end index to create a tree from
 
-    //base case:
-    if (start > end) {
-        return null;
+class Tree {
+    constructor (arr) {
+        this.array = arr;
+        this.root = this.sortedArrayToBST(arr); //?
     }
 
-    //recursive case
+    
+    sortedArrayToBST (arr, start = 0, end = arr.length - 1) { 
+        //takes an array, and a start and end index to create a tree from
 
-    const mid = Math.floor((start + end) / 2); // middle of the array, rounded down 
+        //base case:
+        if (start > end) {
+            return null;
+        }
 
-    const node = new Node (arr[mid]); //the value at the middle of the array is the data
+        //recursive case
 
-    node.left = sortedArrayToBST(arr, start, mid - 1);
-    node.right = sortedArrayToBST(arr, mid + 1, end);
+        const mid = Math.floor((start + end) / 2); // middle of the array, rounded down 
 
-    return node;
+        const node = new Node (arr[mid]); //the value at the middle of the array is the data
+
+        node.left = this.sortedArrayToBST(arr, start, mid - 1);
+        node.right = this.sortedArrayToBST(arr, mid + 1, end);
+
+        return node; //ultimately returns the root!
+    }
+
+    //function takes the root of the BST as input and prints it.
+    prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
+        if (node === null) {
+        return;
+        }
+        if (node.right !== null) {
+        this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+        this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    };
 }
-
-//function takes the root of the BST as input and prints it.
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
  
 
-  const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] //sample
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] //sample
 
-const balancedTree = sortedArrayToBST(myArray);
+const balancedTree = new Tree(myArray);
 
-prettyPrint(balancedTree);
+balancedTree.prettyPrint();
 
-console.log(balancedTree)
+console.log(balancedTree);
